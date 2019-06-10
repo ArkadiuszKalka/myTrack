453df2ca360d85c506f611cdfcf95a63c6da13da
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-06-10 16:46:58 +0200

    Add ServletInitializer class

---
+++ src/main/java/com/kalka/arkadiusz/myTrack/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.kalka.arkadiusz.myTrack;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+        return application.sources(MyTrackApplication.class);
+    }
+}
