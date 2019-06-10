e96fe929f99bb28f9ee12f3ec1a5625dd6279a24
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-06-10 15:58:50 +0200

    Add spring project

---
+++ src/main/java/com/kalka/arkadiusz/myTrack/MyTrackApplication.java
@@ -0,0 +1,13 @@
+package com.kalka.arkadiusz.myTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalka/arkadiusz/myTrack/MyTrackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kalka.arkadiusz.myTrack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTrackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
